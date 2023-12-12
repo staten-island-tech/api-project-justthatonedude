5e6546a7c628be2a220375facc6624fd8cb31433
justthatonedude <jothamgao@gmail.com>
2023-12-12 10:15:24 -0500

    6

--- vite-project/js/GTFS_sort.js
+++ vite-project/js/GTFS_sort.js
@@ -1,7 +1,7 @@
 import {data} from './GTFS_recieve.js'
 
 
-const trips = [];
-data.forEach((trip)=>trips.push(trip.trip))
-
-console.log(trips.trip)
+const trip = data.filter(trips => {
+    return trips 
+})
+console.log(trip); 
